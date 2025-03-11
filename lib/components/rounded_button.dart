9348a4b529c8e216c96a74204df7cea16fe34346
Yutaro <katoyutaro1122@icloud.com>
2025-03-11 15:30:07 +0900

    modularize rounded button component

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ReusableButton extends StatelessWidget {
+
+  final Color buttonColor;
+  final VoidCallback buttonFunction;
+  final String buttonText;
+
+  ReusableButton({required this.buttonColor, required this.buttonFunction, required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: buttonFunction,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            buttonText,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat_app/screens/login_screen.dart';
 import 'package:flash_chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat_app/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -17,22 +19,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
   @override
   void initState() {
     super.initState();
-
     controller = AnimationController(
       vsync: this,
       duration: Duration(seconds: 1),
-      // upperBound: 100.0,
     );
-
     animation = ColorTween(begin: Colors.blueAccent, end: Colors.white).animate(controller);
-
     controller.forward();
-
-    // animation.addStatusListener();
-
     controller.addListener(() {
       setState(() {});
-      print(controller.value);
     });
   }
 
@@ -64,57 +58,35 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
                     ),
                   ),
                 ),
-                Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.black87,
-                    fontWeight: FontWeight.w900,
-                    // decoration: TextDecoration.underline,
-                  ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Flash Chat',
+                      textStyle: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w900,
+                        // decoration: TextDecoration.underline,
+                      ),
+                      speed: Duration(milliseconds: 100)
+                    ),
+                  ],
+                  totalRepeatCount: 1,
                 )
               ],
             ),
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            ReusableButton(
+              buttonColor: Colors.lightBlueAccent,
+              buttonFunction: (){Navigator.pushNamed(context, LoginScreen.id);},
+              buttonText: 'Log In',
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            ReusableButton(
+              buttonColor: Colors.blueAccent,
+              buttonFunction: (){Navigator.pushNamed(context, RegistrationScreen.id);},
+              buttonText: 'Register',
             ),
           ],
         ),
